2885575e967f1f88ba110a84b04bd2e79163f513
saket <saket@saketgupta.com>
2025-07-26 05:21:23 +0530

    Started with the home page

--- TalkByte/client/src/pages/Home.jsx
+++ TalkByte/client/src/pages/Home.jsx
@@ -1,12 +1,25 @@
+
 import React from 'react'
+import { useSelector } from 'react-redux';
+import Sidebar from '../components/Sidebar';
+import NoChatSelected from '../components/NoChatSelected';
+import ChatContainer from '../components/ChatContainer';
 
 const Home = () => {
+  const {selectedUser}=useSelector((state) => state.auth);
   return (
-    <section className='bg-gray-950 w-full min-h-screen text-white pt-20 px-8'>
-      Home Content Here
-      <p className="mt-4">Scroll to see the glassmorphic navbar stay on top.</p>
-      <div className='h-[2000px]'></div> {/* dummy scroll area */}
-    </section>
+    <>
+    <div className='min-h-screen bg-gray-200'>
+      <div className='flex items-center justify-center pt-20 px-4'>
+        <div className='bg-white rounded-lg shadow-md w-full max-w-6xl  h-[calc(100vh-80rem)]'>
+          <div className='flex h-full rounded-lg overflow-hidden'>
+            <Sidebar />
+            {!selectedUser?<NoChatSelected />:<ChatContainer />}
+          </div>
+        </div>
+      </div>
+    </div>
+    </>
   )
 }
 
